bffc5b37eaf2a0b052f9ad87530ebd03d9902c25
Kevin <kevin@moveparallel.com>
2022-12-04 04:28:59 -0800

    day 4 part a

---
+++ aoc2022/day4/camp_cleanup.py
@@ -0,0 +1,51 @@
+"""AOC day 4"""
+
+
+def make_range(str_range: str) -> set[int]:
+    """Given a string representation of a range return the corresponding set
+
+    e.g. "2-4" -> {2, 3, 4}
+    """
+    start, end = map(int, str_range.split("-"))
+    return set(range(start, end + 1))
+
+
+def parse_line(line: str) -> tuple[set[int], set[int]]:
+    """Take an input line and return two ranges
+
+    e.g. `2-4,6-8` -> `({2, 3, 4}, {6, 7, 8})`
+    """
+    range_a, range_b = map(make_range, line.split(","))
+    return range_a, range_b
+
+
+def fully_overlap(set_a: set[int], set_b: set[int]) -> bool:
+    """Check if either set is a subset of the other"""
+    return set_a.issubset(set_b) or set_b.issubset(set_a)
+
+
+def solution_a(input_txt: str) -> int:
+    """solution for part a
+
+    How many assignment pairs fully overlap
+    """
+    overlaps = 0
+    for line in input_txt.strip().split("\n"):
+        range_a, range_b = parse_line(line)
+        overlaps += int(fully_overlap(range_a, range_b))
+
+    return overlaps
+
+
+def solution_b(input_txt: str) -> int:
+    """solution for part b"""
+    score = 0
+    return score
+
+
+if __name__ == "__main__":
+    from aoc2022.utils import read_input
+
+    day4_input = read_input(4)
+    num_overlaps = solution_a(day4_input)
+    print(f"There were {num_overlaps} pairs that overlapped!")
---
+++ tests/test_camp_cleanup.py
@@ -0,0 +1,49 @@
+"""Tests for AOC day 4"""
+import pytest
+
+from aoc2022.day4.camp_cleanup import (
+    fully_overlap,
+    make_range,
+    parse_line,
+    solution_a,
+    solution_b,
+)
+
+TEST_INPUT = """2-4,6-8
+2-3,4-5
+5-7,7-9
+2-8,3-7
+6-6,4-6
+2-6,4-8
+"""
+
+
+def test_make_range():
+    """Test we can make a range from str rep"""
+    assert make_range("2-4") == {2, 3, 4}
+    assert make_range("123-125") == {123, 124, 125}
+
+
+def test_parse_line():
+    """Test a given line returns two sets as expected"""
+    lines = TEST_INPUT.strip().split("\n")
+    assert parse_line(lines[0]) == ({2, 3, 4}, {6, 7, 8})
+    assert parse_line(lines[2]) == ({5, 6, 7}, {7, 8, 9})
+
+
+@pytest.mark.parametrize(
+    "set_a,set_b,overlap",
+    [
+        ({2, 3, 4}, {3, 4}, True),
+        ({3, 4}, {2, 3, 4}, True),
+        ({1, 2, 3}, {4, 5}, False),
+    ],
+)
+def test_fully_overlap(set_a, set_b, overlap):
+    """test given two sets we can determine if either one is a subset of the other"""
+    assert fully_overlap(set_a, set_b) is overlap
+
+
+def test_solution_a():
+    """Test for solution a"""
+    assert solution_a(TEST_INPUT) == 2
