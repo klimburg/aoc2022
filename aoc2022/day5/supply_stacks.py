c84e909760baf2d65ad4c18f0cb0c86f5624d0a8
Kevin <kevin@moveparallel.com>
2022-12-05 11:05:30 -0800

    day 5 part a

---
+++ aoc2022/day5/supply_stacks.py
@@ -0,0 +1,73 @@
+"""AOC day 5"""
+
+
+import re
+
+from aoc2022.utils import read_input
+
+MOVE_PATTERN = re.compile(r"move (\d+) from (\d+) to (\d+)")
+
+
+def parse_input(input_txt: str) -> tuple[list[list[str]], list[str]]:
+    """Split the input text into:
+    * a list of deques representing the stacks of crates
+    * a list of strings for the structions
+    """
+    stack_str, instructions = input_txt.split("\n\n")
+    stack_rows = stack_str.split("\n")
+
+    num_stacks = int(stack_rows[-1].strip().split(" ")[-1])
+    stacks: list[list[str]] = [[] for _ in range(num_stacks)]
+    for row in stack_rows[:-1][::-1]:
+        # iterate from the bottom up
+        for idx in range(num_stacks):
+            # each 3 characters is one crate followed by a space to delimit
+            # chunk in groups of 4, striping whitespace and brackets
+            crate = (
+                row[idx * 4 : (idx + 1) * 4].strip().replace("[", "").replace("]", "")
+            )
+            if crate != "":
+                stacks[idx].append(crate)
+    return stacks, instructions.strip().split("\n")
+
+
+def move_crates(
+    stacks: list[list[str]], num_crates: int, from_stack: int, to_stack: int
+) -> list[list[str]]:
+    """Move some crates from one stack to another"""
+    for _ in range(num_crates):
+        stacks[to_stack].append(stacks[from_stack].pop())
+    return stacks
+
+
+def parse_move(input_str: str) -> tuple[int, int, int]:
+    """Take a string move command
+
+    N.B. stack indexes are zero index while inputs are 1 indexed
+
+    Returns: a tuple of number of crates, from stack index and to stack index
+    """
+    found_values = re.findall(MOVE_PATTERN, input_str)[0]
+    print(found_values)
+    assert len(found_values) == 3
+    num_crates, from_stack, to_stack = map(int, found_values)
+    return num_crates, from_stack - 1, to_stack - 1
+
+
+def solution_a(input_txt: str) -> str:
+    """solution part a"""
+    crates, instructions = parse_input(input_txt)
+    for instruction in instructions:
+        num_crates, from_idx, to_idx = parse_move(instruction)
+        crates = move_crates(crates, num_crates, from_idx, to_idx)
+    return "".join([stack[-1] for stack in crates])
+
+
+def solution_b(input_txt: str) -> str:
+    """solution part b"""
+
+
+if __name__ == "__main__":
+    input_day5 = read_input(5)
+    message = solution_a(input_day5)
+    print(f"Top crates: {message}")
---
+++ tests/test_supply_stacks.py
@@ -0,0 +1,28 @@
+"""tests for day 5"""
+# pylint: disable=missing-function-docstring
+
+from aoc2022.day5.supply_stacks import parse_input, solution_a, solution_b
+
+TEST_INPUT = """
+    [D]    
+[N] [C]    
+[Z] [M] [P]
+ 1   2   3 
+
+move 1 from 2 to 1
+move 3 from 1 to 3
+move 2 from 2 to 1
+move 1 from 1 to 2
+""".lstrip(
+    "\n"
+)
+
+
+def test_parse_input():
+    stacks, instructions = parse_input(TEST_INPUT)
+    print(stacks)
+    assert stacks == [["Z", "N"], ["M", "C", "D"], ["P"]]
+
+
+def test_solution_a():
+    assert solution_a(TEST_INPUT) == "CMZ"
