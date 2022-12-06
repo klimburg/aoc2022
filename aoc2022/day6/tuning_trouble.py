7ccb339ac4f68ad8d9cea5351e614b3619c37306
Kevin <kevin@moveparallel.com>
2022-12-05 22:07:43 -0800

    remove duplicated code

--- aoc2022/day6/tuning_trouble.py
+++ aoc2022/day6/tuning_trouble.py
@@ -1,12 +1,8 @@
 from aoc2022.utils import read_input
 
 
-def solution_a(input_txt: str) -> int:
-    """Find the start of packet marker.
-
-    Identify the first position where the four most
-    recently received characters were all different.
-    """
+def find_end_unique(input_txt: str, n_unique: int):
+    """Find end of a sequence of n unique characters in a longer string"""
     marker = ""
     for idx, char in enumerate(input_txt):
         try:
@@ -14,8 +10,17 @@ def solution_a(input_txt: str) -> int:
             marker = marker[duplicate_index + 1 :] + char
         except ValueError:
             marker += char
-            if len(marker) == 4:
+            if len(marker) == n_unique:
                 return idx + 1
+
+
+def solution_a(input_txt: str) -> int:
+    """Find the start of packet marker.
+
+    Identify the first position where the four most
+    recently received characters were all different.
+    """
+    return find_end_unique(input_txt, 4)
 
 
 def solution_b(input_txt: str) -> int:
@@ -24,15 +29,7 @@ def solution_b(input_txt: str) -> int:
     Identify the first position where the 14 most
     recently received characters were all different.
     """
-    marker = ""
-    for idx, char in enumerate(input_txt):
-        try:
-            duplicate_index = marker.index(char)
-            marker = marker[duplicate_index + 1 :] + char
-        except ValueError:
-            marker += char
-            if len(marker) == 14:
-                return idx + 1
+    return find_end_unique(input_txt, 14)
 
 
 if __name__ == "__main__":
