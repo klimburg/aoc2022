2915e04bb1ed3113db30a31c9921615a9ffcdeae
Kevin <kevin@moveparallel.com>
2022-12-05 22:05:09 -0800

    day 6 part 2

--- aoc2022/day6/tuning_trouble.py
+++ aoc2022/day6/tuning_trouble.py
@@ -19,10 +19,26 @@ def solution_a(input_txt: str) -> int:
 
 
 def solution_b(input_txt: str) -> int:
-    """"""
+    """Find the start of message marker.
+
+    Identify the first position where the 14 most
+    recently received characters were all different.
+    """
+    marker = ""
+    for idx, char in enumerate(input_txt):
+        try:
+            duplicate_index = marker.index(char)
+            marker = marker[duplicate_index + 1 :] + char
+        except ValueError:
+            marker += char
+            if len(marker) == 14:
+                return idx + 1
 
 
 if __name__ == "__main__":
     day_6_input = read_input(6).strip()
-    start_of_message = solution_a(day_6_input)
-    print(f"Message starts at {start_of_message}")
+    start_of_packet = solution_a(day_6_input)
+    print(f"Packet starts at {start_of_packet}")
+
+    start_of_msg = solution_b(day_6_input)
+    print(f"Message starts at {start_of_msg}")
--- tests/test_tuning_trouble.py
+++ tests/test_tuning_trouble.py
@@ -1,6 +1,6 @@
 import pytest
 
-from aoc2022.day6.tuning_trouble import solution_a
+from aoc2022.day6.tuning_trouble import solution_a, solution_b
 
 TEST_INPUTS = [
     "mjqjpqmgbljsphdztnvjfqwrcgsmlb",
@@ -9,9 +9,15 @@ TEST_INPUTS = [
     "nznrnfrfntjfmvfwmzdfjlvtqnbhcprsg",
     "zcfzfwzzqfrljwzlrfnpqdbhtmscgvjw",
 ]
-TEST_RESULTS = [7, 5, 6, 10, 11]
+TEST_RESULTS_A = [7, 5, 6, 10, 11]
+TEST_RESULTS_B = [19, 23, 23, 29, 26]
 
 
-@pytest.mark.parametrize("input,result", (zip(TEST_INPUTS, TEST_RESULTS)))
+@pytest.mark.parametrize("input,result", (zip(TEST_INPUTS, TEST_RESULTS_A)))
 def test_solution_a(input, result):
     assert solution_a(input) == result
+
+
+@pytest.mark.parametrize("input,result", (zip(TEST_INPUTS, TEST_RESULTS_B)))
+def test_solution_b(input, result):
+    assert solution_b(input) == result
