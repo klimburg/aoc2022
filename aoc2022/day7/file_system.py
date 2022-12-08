a95dbdb4f904ecd88a185939986c8a5c4e9379e5
Kevin <kevin@moveparallel.com>
2022-12-07 23:57:57 -0800

    day7 part 1

---
+++ aoc2022/day7/file_system.py
@@ -0,0 +1,56 @@
+from collections import defaultdict
+import os
+
+def solution_a(input_txt: str) -> int:
+    """solve part a
+
+    Find all of the directories with a total size of at most 100000,
+    then calculate the sum of their total sizes.
+    """
+    MAX_DIR_SIZE = 100_000
+    dir_sizes = get_directory_sizes(input_txt)
+    return sum(size for size in dir_sizes.values() if size <= MAX_DIR_SIZE)
+
+
+def get_directory_sizes(input_txt: str) -> dict[str, int]:
+    """Returns a flat dictionary of all directories and their sizes"""
+    file_system: defaultdict[str, int] = defaultdict(int)
+    current_directory = "/"
+    for line in input_txt.strip().split("\n"):
+        if "$ cd" in line:
+            subdir = line.replace("$ cd ", "")
+            if subdir == "/":
+                current_directory = "/"
+            else:
+                current_directory = os.path.abspath(
+                    os.path.join(current_directory, subdir)
+                )
+        elif line == "$ ls":
+            continue
+        else:
+            value_1, value_2 = line.split(" ")
+            if value_1 == "dir":
+                new_dir = os.path.join(current_directory, value_2)
+                file_system[new_dir] += 0
+                assert file_system[new_dir] == 0
+            else:
+                if current_directory == "/":
+                    directories = ["/"]
+                else:
+                    directories = ["/"] + current_directory.split("/")[1:]
+                for index in range(len(directories)):
+                    directory = os.path.join(*directories[: index + 1])
+                    file_system[directory] += int(value_1)
+    return file_system
+
+
+def solution_b(input_txt: str) -> int:
+    """solve part b"""
+    return 0
+
+
+if __name__ == "__main__":
+    from aoc2022.utils import read_input
+
+    day7_input = read_input(7)
+    print("Solution A: ", solution_a(day7_input))
---
+++ tests/test_file_system.py
@@ -0,0 +1,38 @@
+"""Tests for day 7 of AOC 2022"""
+import pytest
+
+from aoc2022.day7.file_system import get_directory_sizes, solution_a, solution_b
+
+TEST_INPUT = """$ cd /
+$ ls
+dir a
+14848514 b.txt
+8504156 c.dat
+dir d
+$ cd a
+$ ls
+dir e
+29116 f
+2557 g
+62596 h.lst
+$ cd e
+$ ls
+584 i
+$ cd ..
+$ cd ..
+$ cd d
+$ ls
+4060174 j
+8033020 d.log
+5626152 d.ext
+7214296 k
+"""
+
+
+def test_get_directory_sizes():
+    expected_dir_sizes = {"/a": 94853, "/d": 24933642, "/a/e": 584, "/": 48381165}
+    assert get_directory_sizes(TEST_INPUT) == expected_dir_sizes
+
+
+def test_solution_a():
+    assert solution_a(TEST_INPUT) == 95437
